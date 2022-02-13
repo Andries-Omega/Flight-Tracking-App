a4a6d97c7e3535b6cbcd1928b865d0fa1ecc6d6a
Andries-Omega <achimule@jhb.dvt.co.za>
2022-02-13 13:33:07 +0200

    added webpackage and leaflet map to project

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+    mode: "development",
+    
+    devServer: {
+    
+    static: "./dist",
+    
+    },
+    
+    module: {
+    
+    rules: [
+    
+    {
+    
+    test: /\.(css)$/,
+    
+    use: ["style-loader", "css-loader"],
+    
+    },
+    
+    ],
+    
+    },
+    
+    };
